e232baf9ea48501fcdfd337a37dc2d34270b9267
Miguel Mercedes <lmyguel0324@gmail.com>
2024-01-05 09:29:55 -0400

    doing query to get specific data

--- myApp.js
+++ myApp.js
@@ -115,8 +115,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ name: 1, age: 0 })
+    .exec((err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
